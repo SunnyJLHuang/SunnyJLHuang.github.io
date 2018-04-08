bd33fd5f934826317b4a7f6d81e58e77debd80ac
SunnyJLHuang <38176719+SunnyJLHuang@users.noreply.github.com>
2018-04-08 15:38:34 +0800

    Add files via upload

---
+++ test-site/scripts/main.js
@@ -0,0 +1,39 @@
+var myHeading=document.querySelector('h1');
+myHeading.innerHTML="Hello world!";
+
+var myImage = document.querySelector('img');
+myImage.onclick=function(){
+	var mySrc=myImage.getAttribute('src');
+	if(mySrc==='images/googletubiao.jpg')
+	{
+		myImage.setAttribute('src','images/IE.png');
+	}
+	else
+	{
+		myImage.setAttribute('src','images/googletubiao.jpg');
+	}
+} 
+
+var myButton=document.querySelector('button');
+var myHeading=document.querySelector('h1');
+
+function setUserName()
+{
+	var myName=prompt('Please enter your name.');             //mark
+	localStorage.setItem('name',myName);
+	myHeading.innerHTML='Mozilla is cool, '+ myName;
+	
+	if(!localStorage.getItem('name'))
+	 {
+		setUserName();
+	 }
+		else
+		{
+			var storedName=localStorage.getItem('name');
+			myHeading.innerHTML='Mozilla is cool, '+storedName;
+		}
+}
+myButton.onclick=function()
+{
+	setUserName();
+}
